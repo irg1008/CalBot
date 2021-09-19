2ed638000ac0659ff660ccd2312a5ca81f4b2e4d
Jorge Ruiz <jorgelinirg@gmail.com>
2021-09-19 18:44:34 +0200

    No me va :|

--- src/gcalendar/getCalendarEvents.ts
+++ src/gcalendar/getCalendarEvents.ts
@@ -25,28 +25,93 @@ async function getCalendarEvents(
   };
 
   try {
-    // Nota de iván:
-    /*
-    Creo que puedes hacer:
-    
-      const res = await axios.get(url, { params });
-      
-    Ya que json está por defecto.
-    */
-    const res = await axios({
-      url,
-      method: "get",
-      headers: { Accept: "application/json" },
+    const res = await axios.get(url, {
       params,
     });
-    console.log(res.data.items);
+    const data: CalendarEventsRes = res.data;
+
+    data.updated = new Date(data.updated);
+
+    for (const item of data.items) {
+      item.created = new Date(item.created);
+      item.updated = new Date(item.updated);
+      item.start = {
+        date: new Date(item.start.date),
+        dateTime: item.start.dateTime,
+        timeZone: item.start.timeZone,
+      };
+
+      item.end = {
+        date: new Date(item.end.date),
+        dateTime: item.end.dateTime,
+        timeZone: item.end.timeZone,
+      };
+    }
+
+    console.log(data);
+
+    return data;
   } catch (err) {
     const e = err as any;
     console.log(e?.response);
   }
 }
 
-interface response {}
+interface Creator {
+  email: string;
+}
+
+interface Organizer {
+  email: string;
+  displayName: string;
+  self: boolean;
+}
+
+interface EventDate {
+  date: Date;
+  dateTime?: Date;
+  timeZone: string;
+}
+
+interface OriginalStartTime {
+  date: string;
+}
+
+interface Item {
+  kind: string;
+  etag: string;
+  id: string;
+  status: string;
+  htmlLink: string;
+  created: Date;
+  updated: Date;
+  summary: string;
+  creator: Creator;
+  organizer: Organizer;
+  start: EventDate;
+  end: EventDate;
+  transparency: string;
+  sequence: number;
+  eventType: string;
+  description: string;
+  location: string;
+  recurrence: string[];
+  recurringEventId: string;
+  originalStartTime: OriginalStartTime;
+}
+
+export interface CalendarEventsRes {
+  kind: string;
+  etag: string;
+  summary: string;
+  description: string;
+  updated: Date;
+  timeZone: string;
+  accessRole: string;
+  defaultReminders: any[];
+  nextSyncToken: string;
+  items: Item[];
+}
 
 getCalendarEvents(
   "AIzaSyB6Nvvdr7EJLNo-1-LVI0gE3mX7QJMv5HY",
