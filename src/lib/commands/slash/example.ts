eaef08454af110f1081e8b895876b701a3dfd675
irg1008 <ivansudevlop@gmail.com>
2021-09-20 11:42:19 +0200

    Should work now

--- src/lib/commands/slash/example.ts
+++ src/lib/commands/slash/example.ts
@@ -3,7 +3,7 @@ import { SlashCommand } from "types/Discord.types";
 
 const command: SlashCommand = {
 	data: new SlashCommandBuilder()
-		.setName("cacota")
+		.setName("example")
 		.setDescription("Replies with cacota!"),
 	execute: async (_, interaction) => {
 		await interaction.followUp({ content: `Una 💩 para ${interaction.user}!` });
